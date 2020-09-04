c82ac3ffdae25b957c9608f86202c58dcfd48679
sho1009 <sho.kayamoto@gmail.com>
2020-09-04 15:51:22 +0900

    ベーシック認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-def render_top_page
+  before_action :basic_auth
+
+  def render_top_page
    render template: "tests/index"
- end
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
+  end
 end
