473e57c4912f35a21dd3cc45e24da3fe2527042e
sho1009 <sho.kayamoto@gmail.com>
2020-09-05 18:36:56 +0900

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,8 @@ class PostsController < ApplicationController
  def index
  end
  
- def create
-   post = Post.create(content: params[:content], checked: false)
-   render json:{ post: post }
- end
+ # def create
+ #   post = Post.create(content: params[:content], checked: false)
+ #   render json:{ post: post }
+ # end
 end
