6b05848bca9652745e4466ed38024357b5982fa6
sho1009 <sho.kayamoto@gmail.com>
2020-09-11 18:25:36 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 40 }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: true
+  validates :passwordpassword_confirmation, presence: true,
 
 end
